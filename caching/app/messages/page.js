b69b527c41a64a65e67468174b949122e74172a2
vlado <vladocavric@gmail.com>
2024-05-26 16:55:23 +0200

    finished caching

--- caching/app/messages/page.js
+++ caching/app/messages/page.js
@@ -1,7 +1,16 @@
+import { unstable_noStore } from 'next/cache';
 import Messages from '@/components/messages';
 
+// export const revalidate =  5 // the same thing as using revalidate in fetch but more elegant
+// export const dynamic = 'force-dynamic' // the same thing as cache: 'no-store'
+
 export default async function MessagesPage() {
+  //unstable_noStore() //the same thing as cache: 'no-store' but is recommended by next team after it exits unstable
   const response = await fetch('http://localhost:8080/messages', {
+    // cache: 'no-store',
+    // next: {
+    //   revalidate: 5,
+    // },
     headers: {
       'X-ID': 'page',
     },
