701a435128ead74eea3c67b69b2260242f9858fb
vlado <vladocavric@gmail.com>
2024-05-26 23:21:35 +0200

    end of optimization and start of auth

---
+++ optimizations/components/header.js
@@ -0,0 +1,27 @@
+import logo from '@/assets/logo.png';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <Link href="/">
+        <Image src={logo} alt="Mobile phone with posts feed on it" 
+        width={80} height={80}
+        // sizes='1vw'
+        priority
+        />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/feed">Feed</Link>
+          </li>
+          <li>
+            <Link className='cta-link' href="/new-post">New Post</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ optimizations/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {hostname: 'res.cloudinary.com'}
+        ]
+    }
+};
+
+export default nextConfig;
